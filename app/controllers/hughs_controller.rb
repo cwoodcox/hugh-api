a9659195746c4bba0f67f0c5358d4aa5d3ff96e3
Corey Woodcox <corey.woodcox@gmail.com>
2014-06-21 21:49:22 -0600

    create a hugh!

--- app/controllers/hughs_controller.rb
+++ app/controllers/hughs_controller.rb
@@ -1,2 +1,18 @@
 class HughsController < ApplicationController
+  respond_to :json
+
+  def create
+    hugh = Hugh.new create_params
+    if hugh.valid?
+      hugh.save
+      respond_with hugh, root: false
+    else
+      render json: { errors: hugh.errors.to_h }, status: :unprocessible_entity
+    end
+  end
+
+  private
+  def create_params
+    params.permit :spark_core_id, :spark_api_access_token
+  end
 end
--- test/controllers/hughs_controller_test.rb
+++ test/controllers/hughs_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class HughsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @request.headers["Accept"] = 'application/json'
+  end
+
+  test "create a Hugh" do
+    post :create, { spark_core_id: "abc123994828939238478", spark_api_access_token: "pXU8sxQ9RpCiZ/FCzPPwjS2JWUX5Uuh7FxFgCYMOwVPUjuaRzgfd" }
+    assert_response 201
+  end
+
 end
