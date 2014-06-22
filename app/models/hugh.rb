14968b055cf2bf4bca1d82ba67d3ba4d8534eeb5
Corey Woodcox <corey.woodcox@gmail.com>
2014-06-22 02:04:40 -0600

    hugh observer for updating the device

--- app/models/hugh.rb
+++ app/models/hugh.rb
@@ -1,2 +1,13 @@
 class Hugh < ActiveRecord::Base
+  def spark_api
+    Faraday.new url: "https://api.spark.io" do |conn|
+      conn.request :authentication, "Bearer", spark_api_access_token
+      conn.adapter Faraday.default_adapter
+    end
+  end
+
+  def spark_color_string
+    red, green, blue = [ color[0..1], color[2..3], color[4..5] ]
+    "#{red},#{green},#{blue},#{brightness}"
+  end
 end
