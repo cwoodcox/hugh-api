adb3012e3b8e9fbd426bec453fab86534004b958
Corey Woodcox <corey.woodcox@gmail.com>
2014-06-22 03:06:52 -0600

    validations on the hugh devices

--- app/models/hugh.rb
+++ app/models/hugh.rb
@@ -1,2 +1,5 @@
 class Hugh < ActiveRecord::Base
+  validates :color, format: /[0-9a-f]{6}/
+  validates :brightness, numericality: true
+  validates :spark_core_id, :spark_api_access_token, :wink_user_id, presence: true
 end
