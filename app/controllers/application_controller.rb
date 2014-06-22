9c9a5463a2ef1fa8ff5af2b4b0e03ef6643a1289
Corey Woodcox <corey.woodcox@gmail.com>
2014-06-22 00:48:43 -0600

    whooooopsies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   protected
   # Override this with a better way to get the current Wink user when we have it
   def current_wink_user_id
-    env['auth.wink.user_id']
+    request.env['auth.wink.user_id']
   end
 end
